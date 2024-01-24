b0b0bc8672a5fdf42cd4818d14fa5b5cbb41ab34
yigiterzen <144549821+yigiterzen@users.noreply.github.com>
2024-01-25 01:08:46 +0300

    Lesson2

---
+++ Lesson2/Lesson2..c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+char bookname[10]="White Fang";
+char author[11]="Jack London";
+char genre[9]="Adventure";
+char page[3]="298";
+char publicationdate[4]="1906";
+
+
+
+printf("Book Introduction\n\n");
+
+printf("Book name:%s\n",bookname);
+printf("Author:%s\n",author);
+printf("Genre:%s\n",genre);
+printf("Page count:%s\n",page);
+printf("Publication date:%s\n",publicationdate);
+
+
+
+
+
+
+
+	return 0;
+}
