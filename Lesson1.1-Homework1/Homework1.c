22c94d5b5a394e30e4ac7c65048a200b4d3e3fa9
yigiterzen <144549821+yigiterzen@users.noreply.github.com>
2024-01-24 18:18:35 +0300

    Lesson1.1 and Homework1

---
+++ Lesson1.1-Homework1/Homework1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf("PERSONAL INFORMATION\n\n");
+	printf("Name: Yigit\n");
+	printf("Surname: Erzen\n");
+	printf("Age: 18\n");
+	printf("Department: Computer Engineering\n");
+	printf("Class: HB-324\n");
+	
+	return 0;
+}
