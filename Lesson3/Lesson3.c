bf82494a3a5a0666514f55050a0e95a6e100cd4f
yigiterzen <144549821+yigiterzen@users.noreply.github.com>
2024-01-27 01:54:36 +0300

    Lesson3

---
+++ Lesson3/Lesson3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+char name[15],surname[15],age[3],country[20],job[10];
+
+printf("Your name:");
+scanf("%s",name);
+
+printf("Your surname:");
+scanf("%s",surname);
+
+printf("Your age:");
+scanf("%s",age),
+
+printf("your country:");
+scanf("%s",country);
+
+printf("your job:");
+scanf("%s",job);
+
+printf("\n\n");
+
+printf("Name:%s\n",name);
+printf("surname:%s\n",surname);
+printf("age:%s\n",age);
+printf("country:%s\n",country);
+printf("job:%s\n",job);
+
+
+
+	return 0;
+}
