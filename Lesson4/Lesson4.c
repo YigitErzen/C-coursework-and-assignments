49c6081f7199fe04fd195ab8261a732f3928c5f8
yigiterzen <144549821+yigiterzen@users.noreply.github.com>
2024-01-28 19:29:26 +0300

    Lesson4

---
+++ Lesson4/Lesson4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+//Grade point average of a student with three exam grades
+int exam1,exam2,exam3,average;
+exam1=70;
+exam2=100;
+exam3=100;
+average=(exam1+exam2+exam3)/3;
+printf("grade average=%d",average);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
