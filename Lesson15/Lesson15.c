5986307b48d89bae6194d77bb9e7d1849f08b21c
Yigit Erzen <144549821+YigitErzen@users.noreply.github.com>
2024-02-09 22:27:08 +0300

    Lesson15

---
+++ Lesson15/Lesson15.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+int number;
+
+printf("Please enter the month number:");
+scanf("%d",&number);
+
+switch(number)	
+{
+case 1: printf("January");
+break;
+
+case 2: printf("February");
+break;
+
+case 3: printf("March");
+break;
+
+case 4: printf("April");
+break;
+
+case 5: printf("May");
+break;
+
+case 6: printf("June");
+break;
+
+case 7: printf("July");
+break;
+
+case 8: printf("August");
+break;
+
+case 9: printf("September");
+break;
+
+case 10: printf("October");
+break;
+
+case 11: printf("November");
+break;
+
+case 12: printf("December");
+break;
+
+default: printf("invalid number");
+break;
+}		
+	return 0;
+}
