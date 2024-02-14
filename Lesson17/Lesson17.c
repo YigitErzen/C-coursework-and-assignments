ad72202e06e4c52d01d67bf2a389c0783c797107
Yigit Erzen <144549821+YigitErzen@users.noreply.github.com>
2024-02-14 23:22:12 +0300

    Lesson17

---
+++ Lesson17/Lesson17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+int numbers[4];
+
+numbers[0]=20;
+numbers[1]=55;
+numbers[2]=75;
+numbers[3]=97;
+
+printf("%d",numbers[2]);
+	
+	return 0;
+}
