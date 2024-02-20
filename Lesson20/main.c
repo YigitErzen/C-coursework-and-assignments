9ea1f3e6d5d19aa0fa7b65749d6833e6a2432dc0
Yigit Erzen <144549821+YigitErzen@users.noreply.github.com>
2024-02-20 23:20:44 +0300

    Lesson20

---
+++ Lesson20/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+int i,x;
+
+for(i=1;i<=5;i++)
+{
+for(x=1;x<=i;x++)
+{
+printf("*");
+}
+printf("\n");	
+}
+
+
+
+
+
+
+
+
+	
+	
+	return 0;
+}
